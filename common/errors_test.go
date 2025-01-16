998b0e74ec370a959bf5db660cf6401c2704b6cf
antanst <antanst@antanst.com>
2025-01-16 10:02:54 +0200

    Add DB scan error

--- common/errors_test.go
+++ common/errors_test.go
@@ -1,24 +1,25 @@
-package common
+package common_test
 
 import (
 	"errors"
 	"fmt"
+	"gemini-grc/common"
 	"testing"
 )
 
 func TestErrGemini(t *testing.T) {
 	t.Parallel()
-	err := NewErrGeminiStatusCode(50, "50 server error")
-	if !errors.As(err, new(*GeminiError)) {
+	err := common.NewErrGeminiStatusCode(50, "50 server error")
+	if !errors.As(err, new(*common.GeminiError)) {
 		t.Errorf("TestErrGemini fail")
 	}
 }
 
 func TestErrGeminiWrapped(t *testing.T) {
 	t.Parallel()
-	err := NewErrGeminiStatusCode(50, "50 server error")
+	err := common.NewErrGeminiStatusCode(50, "50 server error")
 	errWrapped := fmt.Errorf("%w wrapped", err)
-	if !errors.As(errWrapped, new(*GeminiError)) {
+	if !errors.As(errWrapped, new(*common.GeminiError)) {
 		t.Errorf("TestErrGeminiWrapped fail")
 	}
 }
